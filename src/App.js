6379e0ce7bb22a1e8ac2c7bf893634caf1e93444
Cédric David <cedric.david@btopenworld.com>
2018-12-03 15:24:45 +1000

    fix bugs and add marked

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import marked from "marked";
 
-class App extends Component {
+const initialMarkdown = `
+### Headers
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+### List
+
+- list item one
+- list item two
+- list item three
+
+### Links
+
+[FreeCodeCamp](https://learn.freecodecamp.org)
+
+[Google](https://www.google.com "World's Most Popular Search Engine")
+
+### Text Decorations
+
+*italic*
+
+**bold**
+
+***bold and italic***
+
+### Images
+
+![alt text](https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fernando_noronha.jpg/267px-Fernando_noronha.jpg 'Islands in Tropics')
+
+### Blockquote
+
+> To be, or not to be. That is a stupid question.
+
+### Code
+
+\`npm install create-react-app -g\`
+
+\`\`\`
+function addTwoNumbers(a, b) {
+  return a + b
+}
+const name = 'Benjamin'
+const age = 37
+const number = Math.random() * 10
+\`\`\`
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown: initialMarkdown
+    };
+  }
+
+  handleChange = e => this.setState({ markdown: e.target.value });
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Markdown Previewer</h1>
+        <div className="container">
+          <div className="left">
+            <textarea
+              id="editor"
+              value={this.state.markdown}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="right">
+            <div
+              id="preview"
+              dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+            />
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
